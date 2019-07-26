f978e8324fb1b734637ddfedba4dc3841d9bbc36
fano <aafano19@gmail.com>
2019-07-26 11:27:57 +0300

    dernier changement

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 ruby '2.6.3'
 gem 'pry'
 gem 'rubocop', '~> 0.70.0'
+gem 'colorize'
--- app.rb
+++ app.rb
@@ -1,21 +1,23 @@
 require 'bundler'
+require 'colorize'
 Bundler.require
 require_relative 'lib/player'
 require_relative 'lib/game'
 require_relative 'lib/boardcase'
  #les faire entré dans une tableau
 #une initial de bienvenue pour lancer le jeu
-puts "		-------------------------------------------------"
-puts "		|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
-puts "		|Le but du jeu est d'être le dernier survivant !|"
-puts "		-------------------------------------------------"
+puts "		__________________________________________________________"
+puts "		|Bienvenue sur 'ILS VEULENT TOUS MA POO' !               |"
+puts "		|Le but du jeu est d avoir trois croix aligné le premier!|"
+puts "		**********************************************************"
 #faire entre le joueur humain
-puts "  Entré votre nom de joueur 1: "
-print " Le gladiateur  "
-give_name1 = gets.chomp
-puts "  Entré votre nom de joueur 2: "
-print " Le gladiateur  "
-give_name2 = gets.chomp
+puts "  Entré votre nom de joueur 1: ".yellow
+print "🤑 👉 "
+give_name1 = gets.chomp.yellow
+puts "  Entré votre nom de joueur 2: ".blue
+print " 🤖 👉 "
+give_name2 = gets.chomp.blue
 my_game = Game.new(give_name1,give_name2)
 my_game.turn
-# my_game.test_winer
+while my_game.ask_replay == true 	
+end
